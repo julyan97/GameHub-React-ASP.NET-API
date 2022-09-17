3bde6d251dce131107607d6463a3a3522f045902
v.d.vrubchev <v.d.vrubchev@gmail.com>
2022-09-17 15:56:17 +0300

    Add Service for GameEvent

---
+++ Back-End/GameHub.Api/GameHub.BL/Services/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using GameHub.BL.Services.IServices;
+using GameHub.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHub.BL.Services
+{
+    public class BaseService : IBaseService
+    {
+        protected readonly IRepository repository;
+
+        public BaseService(IRepository _repository)
+        {
+            repository = _repository;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await repository.SaveChangesAsync();
+        }
+    }
+}
---
+++ Back-End/GameHub.Api/GameHub.BL/Services/IServices/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameHub.BL.Services.IServices
+{
+    public interface IBaseService
+    {
+        Task SaveChangesAsync();
+    }
+}
