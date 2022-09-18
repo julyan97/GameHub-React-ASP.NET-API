60d0ca1d0ec6d1603af158798c6c47c7529a6e27
julyan997 <julyan997@gmail.com>
2022-09-18 22:18:36 +0300

    SignalR Notification functionality

--- Back-End/GameHub.Api/GameHub.Hub/Hubs/NotificationHub.cs
+++ Back-End/GameHub.Api/GameHub.Hub/Hubs/NotificationHub.cs
@@ -1,19 +1,39 @@
 ﻿using GameHub.SignalR.Hubs.Clients;
 using Microsoft.AspNetCore.SignalR;
+using System.Security.Claims;
 
 namespace GameHub.SignalR.Hubs
 {
     public class NotificationHub : Hub<INotification>
     {
+        public static Dictionary<string, string> UserConnectionIds = new();
 
-        public async Task SendMessage(string message)
-        {
-            await Clients.All.ReceiveMessage(message);
-        }
-
-        public async Task SendNotification()
+        public async Task SendNotificationToAll()
         {
             await Clients.All.NotificationsUpdate();
+        }
+        public async Task AddToGroup(string groupName, string userName)
+        {
+            var userConnectionId = UserConnectionIds[userName];
+            await Groups.AddToGroupAsync(userConnectionId, groupName);
+        }
+
+        public async Task SendNotificationToGroup(string groupName)
+        {
+            await Clients.Group(groupName).NotificationsUpdate();
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var userName = Context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+            var userConnectioniId = Context.ConnectionId;
+
+            if (userName != null)
+            {
+                UserConnectionIds.Add(userName, userConnectioniId);
+            }
+
+            return base.OnConnectedAsync();
         }
     }
 }
